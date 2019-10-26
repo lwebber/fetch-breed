09499f1759b73a8f8d5137eb137bc5d34265fff4
Laura Webber <wwebby1@gmail.com>
2019-10-25 18:55:59 -0700

    remove unused code

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 'use strict';
 
 function watchForms() {
-    $('#multiple').submit(event => {
-        event.preventDefault();
-        let input = $('input[type="text"]').val();
-        getDogImages(input);
-    });
 
     $('#breed').submit(event => {
         event.preventDefault();
@@ -13,27 +8,6 @@ function watchForms() {
         console.log(breed);
         getDogBreedImage(breed);
     });
-
-    function displayResults(responseJson) {
-        let txt = `<img src="${responseJson.message[0]}" class="results-img">`;
-
-        for (let i = 1; i < responseJson.message.length; i++) {
-            txt += `<img src="${responseJson.message[i]}" class="results-img">`;
-        }
-
-        $('.results-images').html(txt);
-
-        //display the results section
-        $('.results').removeClass('hidden');
-    }
-
-    function getDogImages(input) {
-        fetch(`https://dog.ceo/api/breeds/image/random/${input}`)
-            .then(response => response.json())
-            .then(responseJson =>
-                displayResults(responseJson))
-            .catch(error => alert('Something went wrong. Try again later.'));
-    }
 
     function getDogBreedImage(breed) {
         fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
